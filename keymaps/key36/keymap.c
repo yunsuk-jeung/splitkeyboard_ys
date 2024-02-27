542d3fef07e5d38c8a10517ec9ce575fab14fd81
dan <ysjueng@maxst.com>
2024-02-27 10:38:48 +0900

    aa

--- keymaps/key36/keymap.c
+++ keymaps/key36/keymap.c
@@ -9,35 +9,53 @@ enum layer_names {
    _SYM,
    _NVL,//nav
    _NVR,//nav
+   _CO,
    _MO //mouse
 };
-// KC_F1  
-enum {
-  TD_NUM_F1 = 0
+
+enum combos{
+  S6_EQL
+};
+const uint16_t PROGMEM s6_combo[] = {KC_SLASH, KC_6, COMBO_END};
+
+combo_t key_combos[] = {
+  [S6_EQL] = COMBO(s6_combo, KC_EQL),
+};
+
+enum custom_keycodes {
+    M_PRN = SAFE_RANGE,
+    M_BRC,
+    M_CBR,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_QWERTY] = LAYOUT(
-      _______ , _______      , _______     , _______      , _______     , _______      , _______ ,         _______  , _______      , _______     , _______      , _______     , _______         , _______ , 
-      _______ , LT(_FUN,KC_Q ), KC_W        , KC_E         , KC_R        , KC_T         , _______ ,         _______  , KC_Y         , KC_U        , KC_I         , KC_O        , KC_P            , _______ , 
-      _______ , LCTL_T(KC_A) , LGUI_T(KC_S), LALT_T(KC_D) , LSFT_T(KC_F), LCTL_T(KC_G) , _______ ,         _______  , LCTL_T(KC_H) ,LSFT_T(KC_J) , LALT_T(KC_K) , LGUI_T(KC_L), LCTL_T(KC_SCLN) , _______ ,  
-      _______ , LT(_MO,KC_Z) , KC_X        , KC_C         , KC_V        , KC_B         , _______ ,         _______  , KC_N         , KC_M        , KC_COMM      , KC_DOT      , LT(_MO,KC_SLSH ), _______ ,   
-      _______ , _______      , _______     , MO(_NUL)     ,               KC_SPC       , MO(_NVL) ,        MO(_NVR) , KC_SPC       ,               MO(_SYM)     , _______     , _______         , _______),
+      _______ , _______      , _______     , _______      , _______     , _______      , _______ ,         _______  , _______       , _______     , _______      , _______     , _______         , _______ , 
+      _______ , LT(_FUN,KC_Q), KC_W        , KC_E         , KC_R        , KC_T         , _______ ,         _______  , KC_Y          , KC_U        , KC_I         , KC_O        , KC_P            , _______ , 
+      _______ , LCTL_T(KC_A) , LGUI_T(KC_S), LALT_T(KC_D) , LSFT_T(KC_F), LCTL_T(KC_G) , _______ ,         _______  , LCTL_T(KC_H)  ,LSFT_T(KC_J) , LALT_T(KC_K) , LGUI_T(KC_L), LCTL_T(KC_SCLN) , _______ ,  
+      _______ , LT(_MO,KC_Z) , KC_X        , KC_C         , KC_V        , KC_B         , _______ ,         _______  , KC_N          , KC_M        , KC_COMM      , KC_DOT      , LT(_MO,KC_SLSH ), _______ ,   
+      _______ , _______      , _______     , MO(_NUL)     ,               KC_SPC       , MO(_NVL) ,        MO(_NVR) , LT(_CO,KC_SPC),               MO(_SYM)     , _______     , _______         , _______),
     [_NUL] = LAYOUT(
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , _______ , _______ , _______ , _______ , _______ , _______, 
-      _______ , _______ , KC_9    , KC_8    , KC_7    , _______ , _______ ,        _______ , _______ , _______ , _______ , _______ , _______ , _______, 
-      _______ , _______ , KC_6    , KC_5    , KC_4    , _______ , _______ ,        _______ , KC_PGUP , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______, 
-      _______ , _______ , KC_3    , KC_2    , KC_1    , KC_0    , _______ ,        _______ , KC_PGDN , KC_HOME , KC_DEL  , KC_END  , KC_PGDN , _______, 
+      _______ , KC_ASTR , KC_9    , KC_8    , KC_7    , KC_PLUS , _______ ,        _______ , _______ , _______ , M_PRN   , M_CBR   , M_BRC   , _______, 
+      _______ , KC_SLASH, KC_6    , KC_5    , KC_4    , KC_MINS , _______ ,        _______ , KC_PGUP , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______, 
+      _______ , KC_DOT  , KC_3    , KC_2    , KC_1    , KC_0    , _______ ,        _______ , KC_PGDN , KC_HOME , KC_DEL  , KC_END  , _______ , _______, 
       _______ , _______ , _______ , _______ ,           _______ , _______ ,        _______ , _______ ,           _______ , _______ , _______ , _______ ),   
     [_FUN] = LAYOUT(
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , _______ , _______ , _______ , _______ , _______ , _______, 
       _______ , _______ , KC_F1   , KC_F3   , KC_F2   , KC_F5   , _______ ,        _______ , _______ , _______ , _______ , _______ , _______ , _______, 
       _______ , _______ , KC_F4   , KC_F9   , KC_F10  , KC_F11  , _______ ,        _______ , KC_PGUP , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______, 
       _______ , _______ , KC_F7   , KC_F8   , KC_F6   , KC_F12  , _______ ,        _______ , KC_PGDN , KC_HOME , KC_DEL  , KC_END  , KC_PGDN , _______, 
+      _______ , _______ , _______ , _______ ,           _______ , _______ ,        _______ , _______ ,           _______ , _______ , _______ , _______ ),    
+    [_CO] = LAYOUT(
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , _______ , _______ , _______ , _______ , _______ , _______, 
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , KC_ASTR , KC_AMPR , M_PRN   , M_CBR   , M_BRC   , _______, 
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , _______ , KC_UNDS , _______ , _______ , _______ , _______, 
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , _______ , _______ , _______ , _______ , _______ , _______, 
       _______ , _______ , _______ , _______ ,           _______ , _______ ,        _______ , _______ ,           _______ , _______ , _______ , _______ ),   
     [_SYM] = LAYOUT(
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , _______ , _______ , _______ , _______ , _______ , _______, 
-      _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , KC_BSLS , KC_LBRC , KC_RBRC , KC_LPRN , KC_RPRN ,  _______, 
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , KC_BSLS , KC_UNDS , M_PRN   , M_CBR   , M_BRC ,  _______, 
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , KC_TILD , KC_EXLM , KC_AMPR , KC_PIPE , KC_QUOT , _______, 
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,        _______ , KC_AT   , KC_HASH , KC_PERC , KC_CIRC , KC_ASTR , _______, 
       _______ , _______ , _______ , _______ ,           _______ , _______ ,        _______ , _______ ,           _______ , _______ , _______ , _______ ),   
@@ -55,16 +73,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______ , _______ , _______ , _______ ,           _______ , _______ ,         _______ , _______ ,           _______ , _______ , _______ , _______ ), 
     [_NVR] = LAYOUT(
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,         _______ , _______ , _______ , _______ , _______ , _______ , _______, 
-      _______ , _______ , _______ , _______ , _______ , _______ , _______ ,         _______ , KC_EQL  , KC_MINS , KC_UP   , _______ , KC_BSPC , _______, 
+      _______ , _______ , _______ , _______ , _______ , _______ , _______ ,         _______ , KC_ASTR , KC_AMPR , KC_UP   , KC_UNDS , KC_BSPC , _______, 
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,         _______ , KC_PGUP , KC_LEFT , KC_DOWN , KC_RGHT , KC_ENT  , _______, 
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,         _______ , KC_PGDN , KC_HOME , KC_DEL  , KC_END  , KC_F18  , _______, 
       _______ , _______ , _______ , _______ ,           _______ , _______ ,         _______ , _______ ,           _______ , _______ , _______ , _______ ), 
 
     [_MO] = LAYOUT(
       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,         _______ , _______ , _______ , _______ , _______ , _______ , _______ , 
-      _______ , KC_WH_L , KC_WH_R , KC_WH_U , _______ , KC_WH_U , _______ ,         _______ , KC_WH_U , _______ , KC_MS_U , KC_WH_L , KC_WH_R , _______ , 
-      _______ , _______ , KC_MS_L , KC_MS_D , KC_MS_R , KC_WH_D , _______ ,         _______ , KC_WH_D , KC_MS_L , KC_MS_D , KC_MS_R , _______ , _______ , 
-      _______ , _______ , KC_BTN4 , KC_BTN2 , KC_BTN1 , KC_BTN3 , _______ ,         _______ , KC_BTN3 , KC_BTN1 , KC_BTN2 , KC_BTN4 , _______ , _______ , 
+      _______ , KC_WH_L , KC_WH_R , KC_WH_U , KC_WH_U , KC_WH_D , _______ ,         _______ , KC_WH_D , KC_WH_U , KC_MS_U , KC_WH_L , KC_WH_R , _______ , 
+      _______ , _______ , KC_MS_L , KC_MS_D , KC_MS_R , _______ , _______ ,         _______ , _______ , KC_MS_L , KC_MS_D , KC_MS_R , _______ , _______ , 
+      _______ , KC_BTN4 , KC_BTN3 , KC_BTN2 , KC_BTN1 , _______ , _______ ,         _______ , _______ , KC_BTN1 , KC_BTN2 , KC_BTN3 , KC_BTN4 , _______ , 
       _______ , _______ , _______ , _______ ,           _______ , _______ ,         _______ , _______ ,           _______ , _______ , _______ , _______  )
 
   //  [_MOU] = LAYOUT( 
@@ -81,9 +99,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //     KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_F8,         KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,   
   //     KC_LCTL, KC_LALT,  KC_LWIN, KC_LWIN,          KC_SPC,  MO(_FN2),     MO(_FN2), KC_SPC,            MO(_FN2) , KC_F18,  MO(_FN1),   KC_RCTL),
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record)
-{
-   return true;
+// clang-format on
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  switch (keycode) {
+  case M_PRN:  //Types [], {}, or <> and puts cursor between braces.
+    if (record->event.pressed) {
+      SEND_STRING("()");
+      tap_code(KC_LEFT);  //Move cursor between braces.
+    }
+    return false;
+  case M_BRC:  //Types [], {}, or <> and puts cursor between braces.
+    if (record->event.pressed) {
+      SEND_STRING("[]");
+      tap_code(KC_LEFT);  //Move cursor between braces.
+    }
+    return false;
+  case M_CBR:  //Types [], {}, or <> and puts cursor between braces.
+    if (record->event.pressed) {
+      SEND_STRING("{}");
+      tap_code(KC_LEFT);  //Move cursor between braces.
+    }
+    return false;
+  }
+  return true;
 }
-// clang-format on/
--- rules.mk
+++ rules.mk
@@ -11,6 +11,7 @@ BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
+COMBO_ENABLE = yes
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
